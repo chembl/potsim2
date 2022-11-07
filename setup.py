e32e41ddcc1df64a9a45efcc2542e86ac2343ef5
Eloy Felix <eloyfelix@gmail.com>
2022-11-07 11:32:57 +0000

    stop testing Python 3.11

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Topic :: Scientific/Engineering :: Chemistry",
